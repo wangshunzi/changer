0281874767f2c23aa936fd50c36e836cd510f66c
shunzi <shunzi@qq.com>
2016-06-21 20:04:10 +0800

    1.0.0

---
+++ chengerLib/chengerLib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chengerLib
+//
+//  Created by apple on 16/6/21.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
